3277a593c26e1b82cf43bb665c8f7850a71ad996
Komarov Valentin <komarovvv36@gmail.com>
2016-10-07 15:36:58 +0300

    Add requirements

--- check_sites_health.py
+++ check_sites_health.py
@@ -2,10 +2,16 @@ import argparse
 import requests
 import datetime
 
+from urllib.parse import urlparse
+
 from pythonwhois import get_whois
 
 
-DAYS_NUMBER = 30
+QUANTITY_OF_DAYS = 30
+
+
+def get_domain(url):
+    return urlparse(url).netloc
 
 
 def get_date_after_today(days_number):
@@ -19,13 +25,13 @@ def load_urls4check(path):
 
 
 def is_server_respond_with_200(url):
-    response = requests.get('http://{}'.format(url))
+    response = requests.get(url)
     return (response.status_code == 200)
 
 
 def get_domain_expiration_date(domain_name):
     response = get_whois(domain_name)
-    date = get_date_after_today(DAYS_NUMBER)
+    date = get_date_after_today(QUANTITY_OF_DAYS)
     return (response['expiration_date'][0] > date)
 
 
@@ -35,10 +41,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
     urls = load_urls4check(args.path)
     for url in urls:
+        domain_name = get_domain(url)
         print(
             url,
             'Ответил ли сервер 200?',
             is_server_respond_with_200(url),
             'Проплачен ли домен хотя бы на месяц вперед?',
-            get_domain_expiration_date(url)
+            get_domain_expiration_date(domain_name)
         )
