2934678cc7aa52d8d2d874db45700358b4b031c5
Komarov Valentin <komarovvv36@gmail.com>
2016-12-13 02:49:49 +0300

    Refactoring

--- check_sites_health.py
+++ check_sites_health.py
@@ -20,20 +20,42 @@ def get_date_after_today(days_number):
 
 def load_urls4check(path):
     with open(path, 'r') as txt_file:
-        data = [line.strip() for line in txt_file]
-        return data
+        urls = [line.strip() for line in txt_file]
+        return urls
 
 
-def is_server_respond_with_200(url):
+def get_site_status(url):
+    error_log = []
+    domain_name = get_domain(url)
+    statuses = [get_server_response_status(url), get_domain_status(domain_name)]
+    for status in statuses:
+        if not status[0] == 'OK':
+            error_log.append(status[1])
+
+    if not error_log:
+        status = 'OK'
+    else:
+        status = ('FAIL', error_log)
+    return status
+
+
+def get_server_response_status(url):
     response = requests.get(url)
-    return (response.status_code == 200)
+    if response.status_code == 200:
+        status = ('OK', 'Server response with 200')
+    else:
+        status = ('FAIL', 'Server response with {}'.format(response.status_code))
+    return status
 
 
-def get_domain_expiration_date(domain_name):
+def get_domain_status(domain_name):
     response = get_whois(domain_name)
     date = get_date_after_today(QUANTITY_OF_DAYS)
-    return (response['expiration_date'][0] > date)
-
+    if response['expiration_date'][0] > date:
+        status = ('OK', 'Domain doesn\'t expired')
+    else:
+        status = ('FAIL', 'Domain expired')
+    return status
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -41,11 +63,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     urls = load_urls4check(args.path)
     for url in urls:
-        domain_name = get_domain(url)
-        print(
-            url,
-            'Ответил ли сервер 200?',
-            is_server_respond_with_200(url),
-            'Проплачен ли домен хотя бы на месяц вперед?',
-            get_domain_expiration_date(domain_name)
-        )
+        status = get_site_status(url)
+        print('{}............{}!'.format(url, status))
