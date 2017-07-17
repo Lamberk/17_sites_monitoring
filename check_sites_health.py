fc802865011ce9e231ca81e7d3c7aa51399ad838
Komarov Valentin <komarovvv36@gmail.com>
2017-07-17 17:08:08 +0300

    Refactoring after review

--- check_sites_health.py
+++ check_sites_health.py
@@ -5,6 +5,7 @@ import datetime
 from urllib.parse import urlparse
 
 from pythonwhois import get_whois
+from pythonwhois.shared import WhoisException
 
 
 QUANTITY_OF_DAYS = 30
@@ -19,49 +20,47 @@ def get_date_after_today(days_number):
 
 
 def load_urls4check(path):
-    with open(path, 'r') as txt_file:
-        urls = [line.strip() for line in txt_file]
-        return urls
+    with open(path) as txt_file:
+        return [line.strip() for line in txt_file]
 
 
-def get_site_status(url):
-    error_log = []
+def get_site_statuses(url):
     domain_name = get_domain(url)
-    statuses = [get_server_response_status(url), get_domain_status(domain_name)]
-    for status in statuses:
-        if not status[0] == 'OK':
-            error_log.append(status[1])
-
-    if not error_log:
-        status = 'OK'
-    else:
-        status = ('FAIL', error_log)
-    return status
+    return {
+        'response': get_server_response_status(url),
+        'domain': get_domain_status(domain_name)
+    }
 
 
 def get_server_response_status(url):
     response = requests.get(url)
-    if response.status_code == 200:
-        status = ('OK', 'Server response with 200')
-    else:
-        status = ('FAIL', 'Server response with {}'.format(response.status_code))
-    return status
+    return response.status_code == 200
 
 
 def get_domain_status(domain_name):
-    response = get_whois(domain_name)
-    date = get_date_after_today(QUANTITY_OF_DAYS)
-    if response['expiration_date'][0] > date:
-        status = ('OK', 'Domain doesn\'t expired')
+    try:
+        response = get_whois(domain_name)
+    except WhoisException as e:
+        print(e)
     else:
-        status = ('FAIL', 'Domain expired')
-    return status
+        date = get_date_after_today(QUANTITY_OF_DAYS)
+        return response['expiration_date'][0] > date
+
+
+def print_statuses(statuses):
+    for status_type, status in statuses.items():
+        status_str = 'OK' if status else 'FAIL'
+        print(status_type, '{0}!'.format(status_str))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--path', help='Path to file', required=True)
     args = parser.parse_args()
+
     urls = load_urls4check(args.path)
     for url in urls:
-        status = get_site_status(url)
-        print('{}............{}!'.format(url, status))
+        print('#'*80)
+        print(url)
+        status = get_site_statuses(url)
+        print_statuses(status)
